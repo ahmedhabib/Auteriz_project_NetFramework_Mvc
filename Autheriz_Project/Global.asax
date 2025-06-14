295cddee19b92c23e6f5fd84808d0f7184a1031f
Ahmed_Habib <engahmedakram@gmail.com>
2025-06-14 11:16:22 +0300

    Creat_Project

---
+++ Autheriz_Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Autheriz_Project.MvcApplication" Language="C#" %>
---
+++ Autheriz_Project/Models/IdentityModels.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Autheriz_Project.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+        public string Full_Name { get; set; }   
+        public int EmpCode {  get; set; }
+        [ForeignKey(nameof(branch))]
+        public int Branch_id { get; set; }
+
+        public Branch branch { get; set; }
+
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        public DbSet<Branch> Branches { get; set; }
+
+    }
+
+    public class Branch
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Branch_Id { get; set; }
+        public string Branch_Name { get; set; }
+        public ICollection<ApplicationUser> Users { get; set; }
+
+    }
+}
